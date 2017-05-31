26ce4d71598ff75141a951e498953288bb4185ac
selojister <kino_lioz@hotmail.com>
2017-05-31 17:30:07 +0700

    news & news detail mockup & redesign
    - news.html & news.ts
    - news-detail.html

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {IonicPage, ViewController } from 'ionic-angular';
+import {IonicPage, ViewController, NavController } from 'ionic-angular';
+import {NewsDetailPage} from "../news-detail/news-detail";
 
 @IonicPage()
 @Component({
@@ -10,10 +11,14 @@ export class NewsPage {
 
   np: string = "news";
   isAndroid: boolean = false;
-  constructor(public viewCtrl:ViewController) {
+  constructor(public navCtrl: NavController,public viewCtrl:ViewController) {
   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewsPage');
   }
 
+  toDetail(){
+this.navCtrl.push(NewsDetailPage);
+  }
+  
 }
