b1c1f3b4a90c1bb4e377d577883dbd7b97f8de4e
PichiKun <pi.thitikorn_st@tni.ac.th>
2017-06-01 16:06:31 +0700

    Clean code,update Country,Design NoInt
    -Clean some code
    -Make Country modal can display region name
    -simple design NoInternetModal

--- src/pages/schedule-result/schedule-result.ts
+++ src/pages/schedule-result/schedule-result.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
 import {LclBookingPage} from "../lcl-booking/lcl-booking";
-/**
- * Generated class for the ScheduleResultPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+
+
 @IonicPage()
 @Component({
   selector: 'page-schedule-result',
