6427634a73068f4cf832349309af40413ffb941b
PichiKun <pi.thitikorn_st@tni.ac.th>
2017-06-01 17:00:04 +0700

    Change back button function
    Change hardware back button in NoInternetModal
    -press button twice to exit app not modal dismiss

--- src/pages/no-internet-modal/no-internet-modal.ts
+++ src/pages/no-internet-modal/no-internet-modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {ViewController} from 'ionic-angular';
+import {Platform, ViewController, ToastController} from 'ionic-angular';
 import {Network} from "@ionic-native/network";
 
 @Component({
@@ -7,8 +7,23 @@ import {Network} from "@ionic-native/network";
   templateUrl: 'no-internet-modal.html',
 })
 export class NoInternetModalPage {
+  bpress: number = 0;
 
-  constructor(public network: Network,public view: ViewController) {
+
+  constructor(public network: Network,public view: ViewController,public platform: Platform,public toastCtrl: ToastController) {
+    this.platform.ready().then(()=>{
+      this.platform.registerBackButtonAction(()=>{
+        let toast = this.toastCtrl.create({message:'Press back button again to exit',duration:2000,position: 'bottom'});
+        toast.present();
+        this.bpress++;
+        setTimeout(()=>{
+          this.bpress=0;
+        },2000)
+        if(this.bpress==2){
+          this.platform.exitApp();
+        }
+      })
+    })
   }
 
   connected = this.network.onConnect().subscribe(()=>{
