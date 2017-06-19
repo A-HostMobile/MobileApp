7db08a56808e4a0fb7e76c9eafe4923baf1a9b4d
PichiKun <pi.thitikorn_st@tni.ac.th>
2017-06-19 11:34:45 +0700

    validation add item courier

--- src/pages/courier-item-modal/courier-item-modal.ts
+++ src/pages/courier-item-modal/courier-item-modal.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController} from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
+export interface commovalue{
+  va:string;name:string;
+}
 @Component({
   selector: 'page-courier-item-modal',
   templateUrl: 'courier-item-modal.html',
 })
 export class CourierItemModalPage {
   StatusText: String;
-
+  submitted:boolean = false;
+  commodities:commovalue[]=[{va:'cer',name:'Ceramic'},{va:'ex1',name:'Example1'},{va:'ex2',name:'Example2'}];
+  item:{commodity?:string,dw?:number,dl?:number,dh?:number,weight?:number,quantity?:number}={commodity:'cer'};
   constructor(public viewCtrl: ViewController,
               public navParams: NavParams) {
   }
@@ -21,10 +27,14 @@ export class CourierItemModalPage {
     this.viewCtrl.dismiss();
   }
 
-  addItem(){
+  addItem(form:NgForm){
+    this.submitted = true;
+    if(form.valid){
+      console.log(this.item);
       this.viewCtrl.dismiss();
     }
+  }
   editItem(){
- this.viewCtrl.dismiss();
+    this.viewCtrl.dismiss();
   }
 }
