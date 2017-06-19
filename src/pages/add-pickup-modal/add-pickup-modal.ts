33204826bc0c5c34b172122cd240667c4fafbff9
PichiKun <pi.thitikorn_st@tni.ac.th>
2017-06-19 11:51:51 +0700

    validation add pickup

--- src/pages/add-pickup-modal/add-pickup-modal.ts
+++ src/pages/add-pickup-modal/add-pickup-modal.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { ViewController} from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
+export interface cvalue{
+  co:string;cname:string;
+}
 @Component({
   selector: 'page-add-pickup-modal',
   templateUrl: 'add-pickup-modal.html',
 })
 export class AddPickupModalPage {
-
+  submitted:boolean = false;
+  countries:cvalue[]=[{co:'TH',cname:'Thailand'},
+    {co:'HK',cname:'Hongkong'},
+    {co:'JP',cname:'Japan'}];
+  add:{contname?:string,tel?:string,email?:string,address?:string,zip?:string,country?:string}={country:'TH'};
   constructor(public viewCtrl: ViewController) {
   }
 
@@ -14,8 +22,16 @@ export class AddPickupModalPage {
     console.log('ionViewDidLoad AddPickupModalPage');
   }
 
-closemodal() {
-  this.viewCtrl.dismiss();
-}
+
+  closemodal() {
+    this.viewCtrl.dismiss();
+  }
+  addAddress(form: NgForm){
+    this.submitted=true;
+    if(form.valid){
+      console.log(this.add);
+      this.viewCtrl.dismiss();
+    }
+  }
 
 }
