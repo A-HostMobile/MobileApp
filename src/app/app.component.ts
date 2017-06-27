66df52763763fff537a29a1bfbdd80834a1e6a5c
PichiKun <pi.thitikorn_st@tni.ac.th>
2017-06-27 15:17:37 +0700

    fix delele address

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -146,6 +146,9 @@ export class TemplateApp {
   }
 
   ConfirmBox(_Id:any,_Index:any,_pages:any){
+    console.log('alert')
+    let i:number = 0;
+    let j:number = 0;
       this._alert = this.alert.create({
       title: 'Confirm Delete',
       message: 'Do you want to delete this address?',
@@ -161,13 +164,16 @@ export class TemplateApp {
           text: 'Confirm',
           handler: () => {
               console.log('firm')
-              console.log(_pages);
               if(_pages == 'PickupAddressPage'){
+                i++;
                   this.events.publish('deletePickup',_Id,null);
                   console.log('pickup page con')
+                console.log(i);
               }else{
+                j++;
                   this.events.publish('deleteCourierItem',_Id,_Index);
                   console.log('courier page con')
+                console.log(j);
               }
           }
         }
@@ -285,8 +291,6 @@ export class TemplateApp {
       this.ConfirmBox(_Id,_Index,_pages);
     });
   }
-
-
 
   checkStatusLogin(pages:any,params:any){
     this.authService.getProfile().subscribe((res)=>{
