a05800ea974b899808ce5cc79a120bd7e72c260f
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2017-07-05 15:03:45 +0700

    edit service fcm

--- src/providers/fcm-service/fcm-service.ts
+++ src/providers/fcm-service/fcm-service.ts
@@ -30,23 +30,32 @@ export class FcmServiceProvider {
     console.log('party_id from FCMInsertFN: '+FCMParty_id);
     console.log('token from FCMInsertFN: '+FCMtoken);
     return this.http.post(AppSettings.API_ENDPOINT+'token',_body,_options)
-    .map((res:Response) => <any> res.json())
+    .map((res:Response) => {
+      let FCMInsertRes = res.json();
+      if(FCMInsertRes.responseCode == 0){
+        localStorage.setItem('FCMToken',FCMtoken);
+      }
+    })
     .catch(this.handleError);
   }
 
   FCMDeleteTokenFn():Observable<any>{
     let token: any = localStorage.getItem('token');
+    let tokenFCM: any = localStorage.getItem('FCMToken');
 
     let profiles: any = localStorage.getItem('profile');
     let _profile = JSON.parse(profiles);
     let partyId = _profile.p_party_id;
+
+    let _body = new URLSearchParams();
+    _body.append('token', tokenFCM);
 
     let _header = new Headers();
     _header.append( 'Authorization', `Bearer ${token}`);
     _header.append('Content-Type', 'application/x-www-form-urlencoded');
     let _options = new RequestOptions({headers: _header});
 
-    return this.http.delete(AppSettings.API_ENDPOINT+'token/'+partyId,_options)
+    return this.http.put(AppSettings.API_ENDPOINT+'token/'+partyId,_body,_options)
     .map((res:Response) => <any> res.json().responseData)
     .catch(this.handleError);
   }
